38b4665a2905c3df3d729074ad7afbd1c805d647
estevão aguis <estevaosouzaaguis@gmail.com>
2025-10-24 20:43:59 -0300

    Add files via upload

---
+++ atividade1/script.js
@@ -0,0 +1,9 @@
+console.log("aaaaaaaaaaaaaaa"
+);
+document.getElementById("titulo")
+    .innerHTML = "mudando o h1"; 
+    const botão =
+  document.getElementById("botão");
+  botão.addEventListener("click",function (){
+    alert("me mande o verso do seu cartão se não você terá vírus");
+  });
---
+++ atividade2.js
@@ -0,0 +1,14 @@
+const nome = "Estevão"; 
+console.log("nome:", nome); 
+
+let idade = 18;
+console.log("idade:", idade); 
+
+const produto = {
+  nome: "pc", 
+  valor: 4200 
+};
+console.log("Produto original:", produto); 
+
+produto.valor = 4500;
+console.log("Produto com valor modificado:", produto);
